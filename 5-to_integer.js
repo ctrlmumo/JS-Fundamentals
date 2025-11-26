ce70c9dc8a6c0b75ecb359e797582e71d4b52245
Gloria <ctrlmumo11@gmail.com>
2025-11-27 01:01:55 +0300

    Converts argument to an integer

--- 5-to_integer.js
+++ 5-to_integer.js
@@ -3,10 +3,8 @@ const args = process.argv.slice(2);
 if (args != undefined){
     const intValue = parseInt(args[0], 10); // Convert the first argument to an integer using base 10
     if (!isNaN(intValue)) { // Check if the conversion was successful
-        console.log(intValue);
+        console.log(`My number: ${intValue}`);
     } else {
         console.log("Not a number");
     }
-} else{
-    console.log(`My number: ${args}`);
-}
+} 
