f0054a610d9a6a57a421601339b474fb904a19ed
Gloria <ctrlmumo11@gmail.com>
2025-11-27 15:57:23 +0300

    Use for loop to print multiple strings at once

---
+++ 6-multi_languages_loop.js
@@ -0,0 +1,8 @@
+const lines = [ //array containing the three strings
+    "C is fun",
+    "Python is cool",
+    "JavaScript is amazing"
+]
+for (const line of lines) { //loops through each string
+    console.log(line); //each loop iteration prints one line
+}
